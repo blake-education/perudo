9086bc86df549386b903a5dc652184da9150cb69
Wu Qing <wiserfirst@gmail.com>
2017-03-08 09:14:32 +1100

    add test for face value of dice

--- apps/game/test/cup_test.exs
+++ apps/game/test/cup_test.exs
@@ -3,8 +3,17 @@ defmodule CupTest do
 
   test "cup has desired dice count" do
     [1, 2, 3, 4, 5]
-    |> Enum.map(fn dice_count ->
+    |> Enum.each(fn(dice_count) ->
       assert Cup.new(dice_count) |> length == dice_count
+    end)
+  end
+
+  test "dice have face value of 1-6" do
+    [1, 2, 3, 4, 5]
+    |> Enum.map(&Cup.new/1)
+    |> Enum.concat()
+    |> Enum.each(fn(value) ->
+      assert value >=1 and value <= 6
     end)
   end
 end
