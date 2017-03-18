e7a05d57231f495c229e9cc5e4362ce8fd0907b9
Martin Stannard <martin@stannard.id.au>
2017-03-18 23:22:25 +1100

    Fix syntax for registry and copy/paste error

--- apps/game/lib/game.ex
+++ apps/game/lib/game.ex
@@ -13,14 +13,14 @@ defmodule Game do
     # Define workers and child supervisors to be supervised
     children = [
       # Starts a worker by calling: Dossier.Worker.start_link(arg1, arg2, arg3)
-      worker(Registry, {:unique, :table_registry})
+      supervisor(Registry, [:unique, :table_registry])
       # worker(Dossier.Worker, [arg1, arg2, arg3]),
       # supervisor(Dossier.Repo, [])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Dossier.Supervisor]
+    opts = [strategy: :one_for_one, name: Game.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
