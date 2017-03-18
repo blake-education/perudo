1529ddca1ed5a875ba27d7cca2478a361bbc84fb
Martin Stannard <martin@stannard.id.au>
2017-03-18 23:50:46 +1100

    Add table test
    Add a test for Game.Table and add_player and player_count functions

--- apps/game/lib/table.ex
+++ apps/game/lib/table.ex
@@ -10,6 +10,14 @@ defmodule Game.Table do
     {:via, Registry, {:table_registry, id}}
   end
 
+  def player_count(id) do
+    GenServer.call(via_tuple(id), :player_count)
+  end
+
+  def add_player(id, player_id) do
+    GenServer.call(via_tuple(id), {:add_player, id})
+  end
+
   def init(id) do
     {
       :ok,
@@ -18,5 +26,16 @@ defmodule Game.Table do
         players: []
       }
     }
+  end
+
+  def handle_call(:player_count, _from, state) do
+    {:reply, length(state.players), state}
+  end
+
+  def handle_call({:add_player, player_id}, _from, state) do
+    new_state = %{
+      state | players: [player_id] ++ state.players
+    }
+    {:reply, new_state.players, new_state}
   end
 end
