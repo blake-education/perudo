9bf2aa219e8a8e979b640e7fa0042de2fec2c229
Wu Qing <wiserfirst@gmail.com>
2017-03-30 12:34:12 +1100

    update tests

--- apps/game/test/cup_test.exs
+++ apps/game/test/cup_test.exs
@@ -3,17 +3,17 @@ defmodule CupTest do
 
   test "cup has desired dice count" do
     [1, 2, 3, 4, 5]
-    |> Enum.each(fn(dice_count) ->
-      assert Cup.new(dice_count) |> length == dice_count
+    |> Enum.all?(fn(dice_count) ->
+      Cup.new(dice_count) |> length == dice_count
     end)
+    |> assert
   end
 
   test "dice have face value of 1-6" do
     [1, 2, 3, 4, 5]
     |> Enum.map(&Cup.new/1)
     |> Enum.concat()
-    |> Enum.each(fn(value) ->
-      assert value >=1 and value <= 6
-    end)
+    |> Enum.all?(fn(value) -> value >= 1 and value <= 6 end)
+    |> assert
   end
 end
