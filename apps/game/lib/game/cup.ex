2e44f4494a3182ff9ce382b9976ba7353d3fd3a4
Martin Stannard <martin@stannard.id.au>
2017-03-30 13:18:02 +1100

    Change new to generate, add pip counting function

--- apps/game/lib/game/cup.ex
+++ apps/game/lib/game/cup.ex
@@ -3,8 +3,14 @@ defmodule Cup do
   generat a hand of n dice in a cup
   """
 
-  @spec new(integer) :: [integer]
-  def new(n) do
+  @spec generate(integer) :: [integer]
+  def generate(n) do
     for _ <- 1..n, do: :rand.uniform(6)
   end
+
+  @spec count_for(integer, [integer]) :: integer
+  def count_for(pips, dice) do
+    Enum.count(dice, fn(p) -> p == 1 or p == pips end)
+  end
+
 end
--- apps/game/test/cup_test.exs
+++ apps/game/test/cup_test.exs
@@ -4,16 +4,23 @@ defmodule CupTest do
   test "cup has desired dice count" do
     [1, 2, 3, 4, 5]
     |> Enum.all?(fn(dice_count) ->
-      Cup.new(dice_count) |> length == dice_count
+      Cup.generate(dice_count) |> length == dice_count
     end)
     |> assert
   end
 
   test "dice have face value of 1-6" do
     [1, 2, 3, 4, 5]
-    |> Enum.map(&Cup.new/1)
+    |> Enum.map(&Cup.generate/1)
     |> Enum.concat()
     |> Enum.all?(fn(value) -> value >= 1 and value <= 6 end)
     |> assert
+  end
+
+  test "count_for" do
+    assert Cup.count_for(2, [1, 2, 3 ,4]) == 2
+    assert Cup.count_for(2, [1, 1, 1 ,1]) == 4
+    assert Cup.count_for(3, [2, 2, 2 ,2]) == 0
+    assert Cup.count_for(3, [2, 3, 2 ,3]) == 2
   end
 end
