e89a8a6d1344b51274507482cba03446c6c2ef61
Martin Stannard <martin@stannard.id.au>
2017-04-28 16:24:37 +1000

    Update Application
    Move to new style of application layout

--- apps/game/lib/game.ex
+++ apps/game/lib/game.ex
@@ -1,27 +1,12 @@
 defmodule Game do
-  use Application
 
-  @moduledoc """
-  the Game application holds game logic for Perudo
-  """
+  alias Game.Table
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
-  def start(_type, _args) do
-    import Supervisor.Spec, warn: false
+  def join_table do
+    Table.find_or_create(1)
+  end
 
-    # Define workers and child supervisors to be supervised
-    children = [
-      # Starts a worker by calling: Dossier.Worker.start_link(arg1, arg2, arg3)
-      supervisor(Registry, [:unique, :table_registry]),
-      supervisor(Registry, [:unique, :player_registry], id: make_ref())
-      # worker(Game.Worker, [arg1, arg2, arg3]),
-      # supervisor(Game.Repo, [])
-    ]
-
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Game.Supervisor]
-    Supervisor.start_link(children, opts)
+  def table_state(id) do
+    Table.state(id)
   end
 end
