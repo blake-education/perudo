8eeedf892fb171bafaaa908699f421bb043e49dc
Martin Stannard <martin@stannard.id.au>
2017-04-28 16:31:14 +1000

    remove some IO.inspect, warning

--- apps/perudo_web/web/channels/table_channel.ex
+++ apps/perudo_web/web/channels/table_channel.ex
@@ -7,18 +7,13 @@ defmodule PerudoWeb.TableChannel do
 
   def join("table:game", _params, socket) do
     id = Game.join_table()
-    IO.inspect "table:game"
-    IO.inspect id
     new_socket = assign(socket, :table_id, id)
-    IO.inspect new_socket
-    send(self, :after_join)
+    send(self(), :after_join)
     {:ok, new_socket}
   end
 
   def handle_info(:after_join, socket) do
     data = Game.table_state(socket.assigns.table_id)
-    IO.inspect socket
-    IO.inspect data
     push(socket, "state", data)
     {:noreply, socket}
   end
